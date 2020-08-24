c5c39cf5ab7d8e1faf031cd5f4aceba5b25f3772
mmitch2958 <mikescottmitchell@outlook.com>
2020-08-24 18:28:30 -0400

    Forgot autosave was off, updated compoennts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import logo from "./Assets/co-make-logo-v2.png";
 import "./App.css";
+import AddIssue from './Components/AddIssue'
 
 function App() {
 	return (
+
+		
+
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>Co-make is your local hub to resolve issues in your community.</p>
 			</header>
 		</div>
+	
+
 	);
 }
 
---
+++ src/Components/AddIssue.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+
+
+//Function to add inputed form data to final source 
+
+function AddIssue() {
+    
+ 
+//Temp Issue Display Function 
+
+
+const Issues = (props) => {
+    return (
+      <div className="note-list">
+        {props.issues.map((add) => (
+          <div className="note" key={add.id}>
+            <h2>{add.title}</h2>
+                <p>{add.body}</p>
+                <p>{add.image}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
+//Add Issue Form 
+
+const AddForm = (props) => {
+  const [add, setAdd] = useState({
+    title: '',
+      body: '',
+    image: ''
+  });
+
+  const handleChanges = (e) => {
+    console.log("What is target.value", e);
+    setAdd({
+      ...add,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    props.addNewAdd(add);
+    setAdd({
+      title: '',
+      body: '',
+      image: ''
+    });
+  };
+
+  return (
+    <form onSubmit={submitForm}>
+      <label htmlFor="title">Title</label>
+      <input
+        id="title"
+        type="text"
+        placeholder="Enter title"
+        name="title"
+        value={add.title}
+        onChange={handleChanges}
+      />
+
+      <label htmlFor="body">Description Of Issue</label>
+      <textarea
+        id="body"
+        placeholder="Enter Descripton of Issue"
+        name="body"
+        value={add.body}
+        onChange={handleChanges}
+          />
+          
+      <label htmlFor="image">Input Image URL</label>
+      <textarea
+        id="image"
+        placeholder="input Image URL"
+        name="image"
+        value={add.image}
+        onChange={handleChanges}
+      />
+
+
+      <button type="submit"> Add Issue </button>
+    </form>
+  );
+    };
+
+    const [issues, setIssues] = useState([
+        {
+            id: 1,
+            title: 'New Issue to Add',
+            body: 'These are my concerns',
+            image: 'http://www.yourpicturetoadd.pic',
+
+        }
+    ]);
+
+    const addNewIssue = (add) => {
+
+        const newIssue = {
+            
+            id: 1,
+            title: add.title,
+            body: add.body,
+            image: add.image
+            
+        };
+        setIssues([...issues, newIssue]);
+    };
+
+    return (
+        <div>
+            <AddForm addNewIssue={addNewIssue} />
+            <Issues issues={issues} />
+
+        </div>
+    )
+
+}
+
+
+export default AddIssue;
